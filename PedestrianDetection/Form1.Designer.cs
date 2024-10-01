9e871e025ad876469a1e2badcd1706f3c05a907b
DanielZOV <superdanil747@gmail.com>
2024-10-01 11:53:53 +0600

    Добавьте файлы проекта.

---
+++ PedestrianDetection/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace PedestrianDetection
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.btnLoadImages = new System.Windows.Forms.Button();
+            this.btnRecognize = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.pictureBoxBefore = new System.Windows.Forms.PictureBox();
+            this.pictureBoxAfter = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBefore)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxAfter)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnLoadImages
+            // 
+            this.btnLoadImages.Location = new System.Drawing.Point(16, 15);
+            this.btnLoadImages.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnLoadImages.Name = "btnLoadImages";
+            this.btnLoadImages.Size = new System.Drawing.Size(171, 39);
+            this.btnLoadImages.TabIndex = 0;
+            this.btnLoadImages.Text = "Загрузить изображения";
+            this.btnLoadImages.UseVisualStyleBackColor = true;
+            this.btnLoadImages.Click += new System.EventHandler(this.BtnLoadImages_Click);
+            // 
+            // btnRecognize
+            // 
+            this.btnRecognize.Location = new System.Drawing.Point(16, 62);
+            this.btnRecognize.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnRecognize.Name = "btnRecognize";
+            this.btnRecognize.Size = new System.Drawing.Size(171, 39);
+            this.btnRecognize.TabIndex = 1;
+            this.btnRecognize.Text = "Распознать";
+            this.btnRecognize.UseVisualStyleBackColor = true;
+            this.btnRecognize.Click += new System.EventHandler(this.BtnRecognize_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(16, 108);
+            this.btnClear.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(171, 39);
+            this.btnClear.TabIndex = 2;
+            this.btnClear.Text = "Очистить";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.BtnClear_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(13, 318);
+            this.progressBar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(1035, 28);
+            this.progressBar.TabIndex = 3;
+            // 
+            // pictureBoxBefore
+            // 
+            this.pictureBoxBefore.Location = new System.Drawing.Point(195, 15);
+            this.pictureBoxBefore.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBoxBefore.Name = "pictureBoxBefore";
+            this.pictureBoxBefore.Size = new System.Drawing.Size(427, 295);
+            this.pictureBoxBefore.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxBefore.TabIndex = 4;
+            this.pictureBoxBefore.TabStop = false;
+            // 
+            // pictureBoxAfter
+            // 
+            this.pictureBoxAfter.Location = new System.Drawing.Point(616, 15);
+            this.pictureBoxAfter.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBoxAfter.Name = "pictureBoxAfter";
+            this.pictureBoxAfter.Size = new System.Drawing.Size(427, 295);
+            this.pictureBoxAfter.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxAfter.TabIndex = 5;
+            this.pictureBoxAfter.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.pictureBoxAfter);
+            this.Controls.Add(this.pictureBoxBefore);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnRecognize);
+            this.Controls.Add(this.btnLoadImages);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Recognition";
+            this.Load += new System.EventHandler(this.RecognitionForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBefore)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxAfter)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        private System.Windows.Forms.Button btnLoadImages;
+        private System.Windows.Forms.Button btnRecognize;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.PictureBox pictureBoxBefore;
+        private System.Windows.Forms.PictureBox pictureBoxAfter;
+    }
+}
---
+++ PedestrianDetection/Form1.cs
@@ -0,0 +1,212 @@
+﻿using Accord.Imaging;
+using Accord.Imaging.Filters;
+using Microsoft.ML;
+using Microsoft.ML.Data;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace PedestrianDetection
+{
+    public partial class Form1 : Form
+    {
+        private string initialDirectory;
+        private List<string> selectedImagePaths;
+        private bool isLoadImagesEnabled = true;
+        private bool isRecognizeEnabled;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void RecognitionForm_Load(object sender, EventArgs e)
+        {
+            var currentDirectory = Directory.GetCurrentDirectory();
+            initialDirectory = Path.GetFullPath(Path.Combine(currentDirectory, @"..\..\..\..\..\"));
+
+            btnLoadImages.Enabled = isLoadImagesEnabled;
+            btnRecognize.Enabled = isRecognizeEnabled;
+        }
+
+        private void BtnLoadImages_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = initialDirectory;
+                openFileDialog.Filter = "PNG files|*.png";
+                openFileDialog.RestoreDirectory = true;
+                openFileDialog.Multiselect = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    selectedImagePaths = openFileDialog.FileNames.ToList();
+                    DisplayImage(selectedImagePaths.First());
+                }
+                else
+                {
+                    return;
+                }
+            }
+
+            isLoadImagesEnabled = false;
+            UpdateButtons();
+        }
+
+        private void BtnClear_Click(object sender, EventArgs e)
+        {
+            isLoadImagesEnabled = true;
+            isRecognizeEnabled = false;
+            selectedImagePaths = null;
+            pictureBoxBefore.Image = null;
+            pictureBoxAfter.Image = null;
+
+            UpdateButtons();
+        }
+
+        private async void BtnRecognize_Click(object sender, EventArgs e)
+        {
+            progressBar.Minimum = 0;
+            progressBar.Maximum = selectedImagePaths.Count();
+            progressBar.Value = 0;
+
+            var progress = new Progress<int>(value =>
+            {
+                progressBar.Value = value;
+            });
+
+            var mlContext = new MLContext();
+            var hog = new HistogramsOfOrientedGradients();
+            var grayscaleFilter = new Grayscale(0.2125, 0.7154, 0.0721);
+
+            await Task.Run(() =>
+            {
+                int progressValue = 0;
+                foreach (var imagePath in selectedImagePaths)
+                {
+                    var fileNum = Convert.ToInt32(Path.GetFileNameWithoutExtension(imagePath));
+                    var originalImage = new Bitmap(imagePath);
+                    var regions = SplitImageIntoRegions(originalImage, 80, 40);
+
+                    foreach (var region in regions)
+                    {
+                        var regionImage = originalImage.Clone(region, originalImage.PixelFormat);
+                        var grayTestImage = grayscaleFilter.Apply(regionImage);
+
+                        var loadedModel = mlContext.Model.Load($"{initialDirectory}\\model.mdl", out _);
+                        var predictionEngine = mlContext.Model.CreatePredictionEngine<TrainData, ImagePrediction>(loadedModel);
+
+                        var imageData = new TrainData
+                        {
+                            Hog = hog.Transform(regionImage)
+                            .SelectMany(x => x.Descriptor).Select(x => (float)x).ToArray()
+                        };
+
+                        var prediction = predictionEngine.Predict(imageData);
+
+                        if (prediction.PredictedLabel)
+                        {
+                            using (var g = Graphics.FromImage(originalImage))
+                            {
+                                var cropRectangle = new Rectangle(region.X, region.Y, region.Width, region.Height);
+                                g.DrawRectangle(new Pen(Color.Green, 3), cropRectangle);
+                            }
+                        }
+                    }
+
+                    originalImage.Save(initialDirectory + $"Result\\{fileNum}.png");
+
+                    DisplayImageAfterRecognition(initialDirectory + $"Result\\{fileNum}.png");
+
+                    progressValue++;
+                    ((IProgress<int>)progress).Report(progressValue);
+                }
+
+                MessageBox.Show("Распознавание окончено");
+            });
+        }
+
+        private List<Rectangle> SplitImageIntoRegions(Bitmap image, int regionWidth, int step)
+        {
+            var regions = new List<Rectangle>();
+
+            int imageWidth = image.Width;
+            int imageHeight = image.Height;
+
+            for (int x = 0; x < imageWidth; x += step)
+            {
+                if (x + regionWidth > imageWidth)
+                    x = imageWidth - regionWidth;
+
+                var cropRectangle = new Rectangle(x, 0, regionWidth, imageHeight);
+                regions.Add(cropRectangle);
+
+                if (x + regionWidth >= imageWidth)
+                    break;
+            }
+
+            return regions;
+        }
+
+        private void UpdateButtons()
+        {
+            if (!isLoadImagesEnabled)
+                isRecognizeEnabled = true;
+
+            btnLoadImages.Enabled = isLoadImagesEnabled;
+            btnRecognize.Enabled = isRecognizeEnabled;
+        }
+
+        private void DisplayImage(string imagePath)
+        {
+            pictureBoxBefore.Image = System.Drawing.Image.FromFile(imagePath);
+        }
+
+        private async void DisplayImageAfterRecognition(string imagePath)
+        {
+            try
+            {
+                if (File.Exists(imagePath))
+                {
+                    var image = await Task.Run(() => System.Drawing.Image.FromFile(imagePath));
+
+                    if (pictureBoxAfter.InvokeRequired)
+                    {
+                        pictureBoxAfter.Invoke(new Action(() => pictureBoxAfter.Image = image));
+                    }
+                    else
+                    {
+                        pictureBoxAfter.Image = image;
+                    }
+                }
+                else
+                {
+                    MessageBox.Show($"Файл не найден: {imagePath}");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка при отображении изображения: {ex.Message}");
+            }
+        }
+
+
+        public class ImagePrediction
+        {
+            public float Score { get; set; }
+            public bool PredictedLabel { get; set; }
+        }
+
+        public class TrainData
+        {
+            public bool Label { get; set; }
+            [VectorType(3564)]
+            public float[] Hog { get; set; }
+        }
+    }
+}
